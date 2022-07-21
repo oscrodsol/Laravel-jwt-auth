7fb86a225faa6cd6aa9c2db3ed80184bd465c9ad
oscrodsol <oscar43521@gmail.com>
2022-07-21 11:18:16 +0200

    feat: Logout done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -51,4 +51,23 @@ class AuthController extends Controller
     {
         return response()->json(auth()->user());;
     }
+
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+        try {
+            JWTAuth::invalidate($request->token);
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/profile', [AuthController::class, 'me'])->middleware('jwt.auth');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('jwt.auth');
